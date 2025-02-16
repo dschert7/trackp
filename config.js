f20c7340c5530eb461da853dee1c946761a66905
dschert7 <scherten7@gmail.com>
2025-02-15 21:09:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const midiConfig = {
             pageNum: 1,
             pagelabel: "Controls",
             parameters: [
-                { name: "Param1", CC_in: 80, CC_out: 19, range: [0, 127] },
-                { name: "Param2", CC_in: 81, CC_out: 20, range: [0, 127] },
-                { name: "Param3", CC_in: 82, CC_out: 21, range: [0, 127] },
-                { name: "Param4", CC_in: 83, CC_out: 22, range: [0, 127] },
-                { name: "Param5", CC_in: 84, CC_out: 23, range: [0, 127] },
-                { name: "Param6", CC_in: 85, CC_out: 24, range: [0, 127] },
-                { name: "Param7", CC_in: 86, CC_out: 25, range: [0, 127] },
-                { name: "Param8", CC_in: 87, CC_out: 26, range: [0, 127] }
+                { name: "Param2", CC_in: 80, isNRPN: false, CC_out: 19, range: [0, 127] },
+                { name: "Param4", CC_in: 81, isNRPN: false, CC_out: 20, range: [0, 127] },
+                { name: "Param6", CC_in: 82, isNRPN: false, CC_out: 21, range: [0, 127] },
+                { name: "Param8", CC_in: 83, isNRPN: false, CC_out: 22, range: [0, 127] },
+                { name: "Param1", CC_in: 84, isNRPN: false, CC_out: 23, range: [0, 127] },
+                { name: "Param3", CC_in: 85, isNRPN: false, CC_out: 24, range: [0, 127] },
+                { name: "Param5", CC_in: 86, isNRPN: false, CC_out: 25, range: [0, 127] },
+                { name: "Param7", CC_in: 87, isNRPN: false, CC_out: 26, range: [0, 127] }
                 // Add other parameters as needed
             ]
         },
@@ -19,8 +19,8 @@ const midiConfig = {
             pageNum: 2,
             pagelabel: "Effects",
             parameters: [
-                { name: "Param3", CC_in: 82, CC_out: 92, range: [0, 127] },
-                { name: "Param4", CC_in: 83, CC_out: 93, range: [0, 127] }
+                { name: "Param1", CC_in: 80, isNRPN: true, NRPN_val: "0:1", range: [0, 127] },
+                { name: "Param2", CC_in: 81, isNRPN: false, CC_out: 20, range: [0, 127] },
                 // Add other parameters as needed
             ]
         }
